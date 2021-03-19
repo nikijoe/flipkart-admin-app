df0e7540113069796e9a92c1db827b030f4b8f34
Nijo Joseph <nijojoseph@yahoo.com>
2021-03-19 12:50:38 +0530

    changed urlConfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 const baseUrl = 
-    location.hostname === "localhost" ? "http://localhost:2000" : "https://flip-kart-admin-app.herokuapp.com/"
+    location.hostname === "localhost" ? "http://localhost:2000" : "https://flip-kart-admin-app.herokuapp.com"
 export const api = `${baseUrl}/api`
 export const generatePublicUrl =(fileName) => {
     return `${baseUrl}/public/${fileName}`
