98ed7de6a0576d33097d3b71a8f19eb502c25709
Nijo Joseph <nijojoseph@yahoo.com>
2021-04-03 15:45:37 +0530

    changed to glitch

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,7 @@
 const baseUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:2000"
-    : "https://magic-bumpy-raver.glitch.me/";
+    : "https://magic-bumpy-raver.glitch.me";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
