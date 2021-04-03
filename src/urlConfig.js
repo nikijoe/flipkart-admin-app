1216b2ddb2e7adc601b19c4fda9a9851e87ebeac
Nijo Joseph <nijojoseph@yahoo.com>
2021-04-03 15:40:27 +0530

    changed to glitch

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,8 @@
-const baseUrl = 
-    window.location.hostname === "localhost" ? "http://localhost:2000" : "https://flipkart-backend-server.herokuapp.com"
-export const api = `${baseUrl}/api`
-export const generatePublicUrl =(fileName) => {
-    return `${baseUrl}/public/${fileName}`
-}
+const baseUrl =
+  window.location.hostname === "localhost"
+    ? "http://localhost:2000"
+    : "https://magic-bumpy-raver.glitch.me/";
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
