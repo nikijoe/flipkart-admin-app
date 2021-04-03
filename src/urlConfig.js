b438ba452ed5336cdf65f2158b8e51035f93c7b4
Nijo Joseph <nijojoseph@yahoo.com>
2021-04-03 16:51:55 +0530

    changed to heroku

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,10 +1,10 @@
 const baseUrl =
+  //   window.location.hostname === "localhost"
+  //     ? "http://localhost:2000"
+  //     : "https://magic-bumpy-raver.glitch.me";
   window.location.hostname === "localhost"
     ? "http://localhost:2000"
-    : "https://magic-bumpy-raver.glitch.me";
-//   window.location.hostname === "localhost"
-//     ? "http://localhost:2000"
-//     : "https://flipkart-backend-server.herokuapp.com";
+    : "https://flipkart-backend-server.herokuapp.com";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
